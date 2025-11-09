eb193d102cf6e87c08095e574800a0baf20d260b
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-11-09 23:43:57 +0100

    use effect

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,5 +1,24 @@
+import React, {useState} from "react"
+
 function MyComponent () {
-    return()
+    const [name, setName] = useState();
+    const [age, setAge] = useState(0);
+
+    const updateName = () => {
+        setName("Spongebob");
+    }
+
+    const incrementAge = () => {
+        setAge(age + 1);
+    }
+
+    return( <div> 
+        <p>Name: {name}</p>
+        <button onClick={updateName}>Set Name</button>
+
+        <p>Age: {age}</p>
+        <button onClick={incrementAge}>Increment Age</button>      
+    </div>);
 }
 
 export default MyComponent
