8a3fce4af36f8fcfcff7f4acd691ac7ec84bd50f
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-11-09 23:55:52 +0100

    use effect

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -3,6 +3,7 @@ import React, {useState} from "react"
 function MyComponent () {
     const [name, setName] = useState();
     const [age, setAge] = useState(0);
+    const [isEmployed, setIsEmployed] = useState(false);
 
     const updateName = () => {
         setName("Spongebob");
@@ -12,12 +13,19 @@ function MyComponent () {
         setAge(age + 1);
     }
 
+    const toggleEmployed = () => {
+        setIsEmployed(!isEmployed);
+    } 
+
     return( <div> 
         <p>Name: {name}</p>
         <button onClick={updateName}>Set Name</button>
 
         <p>Age: {age}</p>
-        <button onClick={incrementAge}>Increment Age</button>      
+        <button onClick={incrementAge}>Increment Age</button> 
+
+        <p>Is employed: {isEmployed ? "Yes" : "No"}</p>
+        <button onClick={toggleEmployed}>Increment Age</button>      
     </div>);
 }
 
