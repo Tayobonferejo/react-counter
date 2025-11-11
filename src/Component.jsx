d2c4fb3ab4741c756add5c1fbca4e2edfec5624b
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-11-11 23:44:37 +0100

    component file

---
+++ src/Component.jsx
@@ -0,0 +1,9 @@
+import React, {useState} from "react";
+
+function Component() {
+    const [car, setCar] = useState({year: 2024, make: "Ford", model: "Mustang"});
+    return (<>
+    </>)
+}
+
+export default Component
