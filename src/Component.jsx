a61315bb2a0010ad241e1ee7a2f94c463d8a87f1
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-11-11 23:50:40 +0100

    color picker

--- src/Component.jsx
+++ src/Component.jsx
@@ -2,8 +2,9 @@ import React, {useState} from "react";
 
 function Component() {
     const [car, setCar] = useState({year: 2024, make: "Ford", model: "Mustang"});
-    return (<>
-    </>)
+    return (<div>
+        <p>Your favorite car is: {car.year} {car.make} {car.model}</p>
+    </div>)
 }
 
 export default Component
