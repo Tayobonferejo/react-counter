7a5469614dc2a8aa9b341ceb54b98b3e27fbfd04
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-11-13 22:39:27 +0100

    component file

--- src/Component.jsx
+++ src/Component.jsx
@@ -4,6 +4,7 @@ function Component() {
     const [car, setCar] = useState({year: 2024, make: "Ford", model: "Mustang"});
     return (<div>
         <p>Your favorite car is: {car.year} {car.make} {car.model}</p>
+        <input type="number" value={car.year} onChange={}></input>
     </div>)
 }
 
