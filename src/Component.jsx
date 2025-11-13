c35c8d986d25a68d588dcb4ea4272bf5179a5ce8
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-11-13 22:41:17 +0100

    component file

--- src/Component.jsx
+++ src/Component.jsx
@@ -2,9 +2,13 @@ import React, {useState} from "react";
 
 function Component() {
     const [car, setCar] = useState({year: 2024, make: "Ford", model: "Mustang"});
+
+    function handleYearChange(event) {
+
+    }
     return (<div>
         <p>Your favorite car is: {car.year} {car.make} {car.model}</p>
-        <input type="number" value={car.year} onChange={}></input>
+        <input type="number" value={car.year} onChange={handleYearChange}></input>
     </div>)
 }
 
